6f364217163aa18f4198ba6e6a26685ca1db8b1d
Milos Popovic <milos_popovic@outlook.com>
2020-07-20 19:01:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(this_name)
+    @name = this_name
+  end
+
+  def job=(this_job)
+    @job = this_job
+  end
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+end
